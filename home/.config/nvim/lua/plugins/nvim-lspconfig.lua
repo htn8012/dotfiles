aa83346fbb576eded8dc7b4e71659ae6227627d2
htn <htn5299@gmail.com>
2025-01-02 23:02:32 +0700

    update

--- home/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ home/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -6,12 +6,14 @@ local M = {
 		{ "williamboman/mason-lspconfig.nvim" },
 		{ "saghen/blink.cmp" },
 		{
+			"j-hui/fidget.nvim",
+			opts = {},
+		},
+		{
 			"folke/lazydev.nvim",
 			ft = "lua", -- only load on lua files
 			opts = {
 				library = {
-					-- See the configuration section for more details
-					-- Load luvit types when the `vim.uv` word is found
 					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
 				},
 			},
